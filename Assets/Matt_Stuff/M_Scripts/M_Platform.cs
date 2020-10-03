6c62041a349b8c5ae8e452b6e3a24ca1c0ed4772
matthew3900 <mthomp28@asu.edu>
2020-10-03 13:42:32 -0700

    Endless Prefab Generation / Cycling
    Prefabs now spawn and recycle in a predefined order shortly after the player passes through a section. Working on random ordering and generation rather than using a preset order

---
+++ Assets/Matt_Stuff/M_Scripts/M_Platform.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class M_Platform : MonoBehaviour
+{
+
+    private M_PlatformManager manager;
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        Debug.Log("Collision Enter");
+    }
+
+    private IEnumerator OnTriggerExit(Collider other)
+    {
+        Debug.Log("CollisionExit");
+        if (other.CompareTag("Player"))
+        {
+            yield return new WaitForSeconds(1.5f);
+            Debug.Log("Reached end of platform");
+            manager.RecyclePlatform(this.transform.parent.gameObject);
+        }
+    }
+
+    private void OnEnable()
+    {
+        manager = GameObject.FindObjectOfType<M_PlatformManager>();
+    }
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Matt_Stuff/M_Scripts/M_PlatformManager.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class M_PlatformManager : MonoBehaviour
+{
+    [SerializeField] private GameObject[] platformPrefabs;
+    [SerializeField] private float prefabLength = 100;
+    [SerializeField] private float offset = 0;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        for (int i = 0; i < platformPrefabs.Length; i++)
+        {
+            Instantiate(platformPrefabs[i], new Vector3(0, 0, prefabLength * i), Quaternion.Euler(0, 0, 0));
+            offset += prefabLength;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void RecyclePlatform(GameObject platform)
+    {
+        platform.transform.position = new Vector3(0, 0, offset);
+        offset += prefabLength;
+    }
+
+}
